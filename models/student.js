d12e5640e519681b56619ec8dd87bce4101ee8b2
gulab <mdg122112@gmail.com>
2026-05-19 07:38:15 +0530

    first commit

--- models/student.js
+++ models/student.js
@@ -1,1 +1,21 @@
-Student.js
+const mongoose = require("mongoose");
+
+const studentSchema = new mongoose.Schema({
+
+    name: String,
+
+    father: String,
+
+    dob: String,
+
+    className: String,
+
+    phone: String,
+
+    password: String,
+
+    username: String
+});
+
+module.exports =
+mongoose.model("Student", studentSchema);
